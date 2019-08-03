74b37000476f7970a0694599f4d9a17e85ec4e71
Ulric Ye <yye90012@gmail.com>
2019-08-03 15:29:49 -0700

    Refactored middleware

---
+++ middleware/index.js
@@ -0,0 +1,48 @@
+var middlewareObj = {};
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+
+middlewareObj.isLoggedIn = function(req, res, next) {
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+middlewareObj.checkCampgroundOwnership = function(req, res, next) {
+  if(req.isAuthenticated()) {
+    Campground.findById(req.params.id, function(err, foundCg) {
+      if(err) {
+        res.redirect("back");
+      } else {
+        if(foundCg.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
+}
+
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+  if(req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+      if(err) {
+        res.redirect("back");
+      } else {
+        if(foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
+}
+
+module.exports = middlewareObj
