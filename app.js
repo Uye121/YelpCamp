b08e93ff48eb003ba1abfefb0415bea1695d6fda
Ulric Ye <yye90012@gmail.com>
2019-08-30 22:59:31 -0700

    Added MongoDB Atlas

--- app.js
+++ app.js
@@ -17,7 +17,12 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true, useCreateIndex: true }).then(() => {
+  console.log("connected to DB");
+}).catch(err => {
+  console.log(err);
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
